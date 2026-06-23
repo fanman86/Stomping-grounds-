4e4fc683c4a2385c451f672ac58cf57899c549e0
fanman86 <likumbivusumuzhi@gmail.com>
2026-06-23 22:00:24 +0200

    23/06/2026

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,3 @@
 function greetuser(){
 document.getElementById("sentence")
-.innerHtml="Hi there!<br>want to know more aboutl</br> artizell? click the button below";}
+.innerHTMl="Hi there!<br>want to know more aboutl</br> artizell? click the button below";}
