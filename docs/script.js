5500071550a1e06d57c204c86dae07cc66a746b1
fanman86 <likumbivusumuzhi@gmail.com>
2026-06-27 04:56:13 +0200

    27/06/2026

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
 function greetuser(){
-document.getElementById("name")
-;}
+     let name=document.getElementById("name").value
+     if(name!==""){document.getElementById("Aboutartizell").style.display="block";
+     }
+    }
+  
