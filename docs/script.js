9ad6f549dbad50e2ddcc188085afc30a9b7f9419
fanman86 <likumbivusumuzhi@gmail.com>
2026-07-09 07:41:31 +0200

    09/07/2026

--- docs/script.js
+++ docs/script.js
@@ -5,4 +5,5 @@ function greetuser(){
 function ShowButton(){
        let name=document.getElementById("name").value
        if(name!==""){document.getElementById("Aboutbutton").style.display="blocked"}
-     }
+       if(name===""){document.getElementById("name").innerHTML="Hello"+""+"name"+"want to know about artizell?then click the button below"}}
+
