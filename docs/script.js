da409fc74b0d0f2c4156639e5af87f87cae4404c
fanman86 <likumbivusumuzhi@gmail.com>
2026-07-13 06:46:05 +0200

    13/07/2026

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,12 @@
 function greetUser(){
    let name=document.getElementById("name").value.trim();
-         If(name!==""){
+         If(name!=="");{
             document.getElementById("sentence").innerHTML="Welcome"+name+"We're glad you're here!!";
-            document.getElementById("AboutButton").display.style="inline-block";}}
-
-
-
+            document.getElementById("AboutButton").display.style="inline-block";
+            document.getElementById("name").display.style="none";
+         }
+         else{document.getElementById("name").innerHtml=
+               "please enter your name"
+            }
+      }
+          
