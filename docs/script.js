2f05882d6ad1bd88de78c46d73885d114763f1a6
fanman86 <likumbivusumuzhi@gmail.com>
2026-07-16 06:58:39 +0200

    16/07/2026

--- docs/script.js
+++ docs/script.js
@@ -4,6 +4,7 @@ function greetUser(){
             document.getElementById("AboutButton").style.display="inline-block";
             document.getElementById("name").innerHTML="none";
             document.getElementById("sentence").innerHTML="Hello "+name+"! we're glad you're here!"
+            document.getElementById("continueButton").style.display="block"
          }
          else{
               document.getElementById("name").innerHTML="please enter name";
