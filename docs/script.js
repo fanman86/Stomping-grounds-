0e1119fe94cea1142b2040dc9f5d356b46955096
fanman86 <likumbivusumuzhi@gmail.com>
2026-07-20 21:47:23 +0200

    20/07/2026

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,18 @@
 function greetUser(){
    let name=document.getElementById("name").value.trim();
          if(name!==""){
-            document.getElementById("AboutButton").style.display="inline-Block"
             document.getElementById("sentence").innerHTML="Hello "+name+"! we're glad you're here!";
             document.getElementById("continueButton").style.display="none";
            }
-   let about=document.getElementById("AboutButton")
-      about.style.display.opacity
+   let about=document.getElementById("AboutButton");
+      about.style.display="inline-Block"
+      about.style.opacity="1"
+   let input=document.getElementById("name");
+       input.style.opacity="0"
     
-   let input=document.getElementById("name").style.display.opacity="0";
+
          else{
               document.getElementById("sentence").innerHTML="welcome to artizell<br>please enter your name";
              }
             }
-    
+   
