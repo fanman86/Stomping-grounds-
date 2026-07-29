e254d20eca1e98f3fd5e26306cee42089fdf7cb2
fanman86 <likumbivusumuzhi@gmail.com>
2026-07-29 06:38:34 +0200

    Fixing some bugs.

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,7 @@ function greetUser(){
           let welcoming=document.getElementById("sentence");
           welcoming.style.opacity="0";
           setTimeout(function(){
-                                welcoming.innerHTML="welcome "+name+" !we're glad you're here !";
+                                welcoming.innerHTML="welcome "+name+"! we're glad you're here!";
                                 welcoming.style.opacity="1";
           },500);
          let about=document.getElementById("AboutButton");
@@ -12,11 +12,11 @@ function greetUser(){
          about.style.opacity="1";
          about.style.transform="translateY(0)";
          let input=document.getElementById("name");
-         let contin=coninueButton.getElementById("continueButton");
+         let contin=document.getElementById("continueButton");
          input.style.opacity="0";
          contin.style.opacity="0";
          setTimeout(function(){
-                              document.getElementById("name").input.style.display="none";
+                              document.getElementById("name").style.display="none";
                               document.getElementById("continueButton").style.display="none";
                              },500);}
 
